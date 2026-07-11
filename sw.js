ebc890a7e040ca8eafdc1f26108534457d69a145
cody7bailey-bit <cody7bailey@gmail.com>
2026-07-10 20:43:31 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lineup-v1';
+const CACHE = 'lineup-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
